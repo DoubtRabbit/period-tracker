07c0791bb77446478a2dc5745bc2fb490769989e
DoubtRabbit <amelka3911@gmail.com>
2024-06-16 13:31:00 -0400

    commit

--- pages/main.py
+++ pages/main.py
@@ -1,13 +1,25 @@
 
 import streamlit as st
 
+# running the page
 def run():
     st.header("FIRST PAGE")
     st.write("This is the first page in our Streamlit app!")
 
+# setting background & stuff
+custom_css = """
+<style>
+body {
+    background-color: #f0f0f0; /* Set your desired background color here */
+}
+</style>
+"""
+# rendering custom_css
+st.markdown(custom_css, unsafe_allow_html=True)
+
 # Set up the app title and sidebar
 st.set_page_config(page_title="My Streamlit App", page_icon=":rocket:", layout="wide")
-st.title("Welcome to My Multi-Page Streamlit App")
+st.title("Crave")
 
 # Sidebar for navigation
 st.sidebar.title("Navigation")
